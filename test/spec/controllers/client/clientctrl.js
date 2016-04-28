f166f238e231fe45a14ab3daf740b777e485f924
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2016-04-27 22:43:17 -0300

    Api Clients Minutrade testes clientctrl resolvidos.

--- test/spec/controllers/client/clientctrl.js
+++ test/spec/controllers/client/clientctrl.js
@@ -1,48 +1,62 @@
 /**
  * Created by alex on 22/04/16.
  */
+'use strict';
 describe('ClientCtrl', function() {
-	beforeEach(module('api-clients-minutrade'));
-
 	var $controller;
+    var client;
+
+    beforeEach(function() {
+        module('api-clients-minutrade');
+
+        module(function($provide) {
+
+            $provide.value('client', {
+                client: function() {
+                    return {};
+                }
+            });
+            return null;
+        });
+    });
+
 
 	beforeEach(inject(function(_$controller_){
 		// The injector unwraps the underscores (_) from around the parameter names when matching
 		$controller = _$controller_;
 	}));
 
-	//describe('$scope.addPhoneNumber', function() {
-	//	it('Add an element in phone_numbers', function() {
-	//		var $scope = {};
-	//		var controller = $controller('ClientCtrl', { $scope: $scope });
-	//		console.log($controller)
-	//		$scope.phone_numbers = [''];
-	//		expect($scope.phone_numbers.length).toEqual(1);
-	//		$scope.addPhoneNumber();
-	//		expect($scope.phone_numbers.length).toEqual(2);
-	//	});
-	//});
+	describe('$scope.addPhoneNumber', function() {
+		it('Add an element in phone_numbers', function() {
+            var $scope = {};
+			var controller = $controller('ClientCtrl', { $scope: $scope});
+			$scope.phone_numbers = [''];
+			expect($scope.phone_numbers.length).toEqual(1);
+			$scope.addPhoneNumber();
+			expect($scope.phone_numbers.length).toEqual(2);
+		});
+	});
 
-	//describe('$scope.removePhoneNumber', function() {
-	//	it('Delete an element in phone_numbers', function() {
-	//		var $scope = {};
-	//		var controller = $controller('ClientCtrl', { $scope: $scope });
-	//		$scope.phone_numbers = [''];
-	//		expect($scope.phone_numbers.length).toEqual(1);
-	//		$scope.addPhoneNumber();
-	//		expect($scope.phone_numbers.length).toEqual(2);
-	//		$scope.removePhoneNumber();
-	//		expect($scope.phone_numbers.length).toEqual(1);
-	//	});
-	//});
-	//
-	//describe('$scope.close', function() {
-	//	it('Cancel the alert', function() {
-	//		var $scope = {};
-	//		var controller = $controller('ClientCtrl', { $scope: $scope });
-	//		$scope.alert = {};
-	//		$scope.close();
-	//		expect($scope.alert).toEqual(undefined);
-	//	});
-	//});
+	describe('$scope.removePhoneNumber', function() {
+		it('Delete an element in phone_numbers', function() {
+			var $scope = {};
+			var controller = $controller('ClientCtrl', { $scope: $scope });
+			$scope.phone_numbers = [''];
+			expect($scope.phone_numbers.length).toEqual(1);
+			$scope.addPhoneNumber();
+			expect($scope.phone_numbers.length).toEqual(2);
+			$scope.removePhoneNumber();
+			expect($scope.phone_numbers.length).toEqual(1);
+		});
+	});
+
+	describe('$scope.close', function() {
+		it('Cancel the alert', function() {
+			var $scope = {};
+			var controller = $controller('ClientCtrl', { $scope: $scope });
+			$scope.alert = {};
+			$scope.close();
+			expect($scope.alert).toEqual(undefined);
+		});
+	});
 });
